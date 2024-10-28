23248b8038dd4c168b72abdf543f09eb707f75fb
Rishitha2814 <cherukurishitha081@gmail.com>
2024-10-28 18:49:45 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,15 @@
+import random 
+import string
+
+length=int(input("enter length:"))
+chars =string.ascii_letters
+chars +=string.digits
+chars +=string.punctuation
+ 
+password= ""
+for i in range(length):
+  password +=random.choice(chars)
+print("your random password is:",password)
+
+  
+
